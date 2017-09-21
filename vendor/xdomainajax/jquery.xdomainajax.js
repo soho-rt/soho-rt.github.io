25c9c25169812a57a2ce20d7c43138a78a79c845
Lucas Pereira Brígida <grillobright@gmail.com>
2017-09-21 01:41:09 -0300

    Preview gif and web proxy domain updated

--- vendor/xdomainajax/jquery.xdomainajax.js
+++ vendor/xdomainajax/jquery.xdomainajax.js
@@ -1,2 +1,2 @@
-var COLIBRI_PROXY = 'https://desolate-lowlands-5734.herokuapp.com/scraper?url=';
+var COLIBRI_PROXY = 'https://colibri-web-proxy.herokuapp.com/scraper?url=';
 //var COLIBRI_PROXY = 'http://localhost:3000/scraper?url=';
